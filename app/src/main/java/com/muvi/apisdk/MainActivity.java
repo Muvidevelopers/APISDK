60d0e08a98a04a2e9b383119874860f0ca27a88f
alok1984 <alok.oca@gmail.com>
2017-09-05 16:09:55 +0530

    merge commit

--- app/src/main/java/com/muvi/apisdk/MainActivity.java
+++ app/src/main/java/com/muvi/apisdk/MainActivity.java
@@ -3,6 +3,8 @@ package com.muvi.apisdk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.home.apisdk.apiController.SDKInitializer;
+
 public class MainActivity extends AppCompatActivity implements SDKInitializer.SDKInitializerListner{
     public static final String APP_TOKEN = "your token";
 
@@ -10,7 +12,7 @@ public class MainActivity extends AppCompatActivity implements SDKInitializer.SD
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       new SDKInitializer().init(this,APP_TOKEN);
+       SDKInitializer.getInstance().init(this,this,APP_TOKEN);
     }
 
     @Override
