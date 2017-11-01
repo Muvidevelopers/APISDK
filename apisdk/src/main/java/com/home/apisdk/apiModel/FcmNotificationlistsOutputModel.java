51d031b41d9046855b1e27decf4df5b7ceb84cf6
Muvidevelopers <abhishek@muvi.com>
2017-11-01 14:44:15 +0530

    Implementation of FCM Notification API and added some new functionalities which was not present in APISDK but present in VOD

--- apisdk/src/main/java/com/home/apisdk/apiModel/FcmNotificationlistsOutputModel.java
+++ apisdk/src/main/java/com/home/apisdk/apiModel/FcmNotificationlistsOutputModel.java
@@ -1,16 +1,28 @@
 package com.home.apisdk.apiModel;
 
 /**
- * Created by Android on 10/29/2017.
+ * This Model Class Holds All The Output Attributes For FcmNotificationcountAsynTask.
  */
 
 public class FcmNotificationlistsOutputModel {
 
     private String notification = "";
 
+    /**
+     * This Method is use to Get the Notification
+     *
+     * @return notification
+     */
+
     public String getNotification() {
         return notification;
     }
+
+    /**
+     * This Method is use to Set the Notification
+     *
+     * @param notification For Setting The Notification
+     */
 
     public void setNotification(String notification) {
         this.notification = notification;
